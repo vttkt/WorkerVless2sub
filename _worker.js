8491af5e648030a3214d4f20413717fe1bfb8623
vttkt <166114184+vttkt@users.noreply.github.com>
2024-04-16 20:32:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,7 +11,6 @@ let addresses = [
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt',//可参考内容格式 自行搭建。 
 	'https://wenben-1fl.pages.dev/IP.txt?token=mykv13666', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 ];
